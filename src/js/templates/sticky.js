169219efa8d8da75c59cf1fe31a2fbb9432e33b7
Максим <deputat_maksym@i.ua>
2019-09-11 19:27:02 +0300

    article final

--- src/js/templates/sticky.js
+++ src/js/templates/sticky.js
@@ -1,11 +1,29 @@
-// window.addEventListener('load', function(e) {
+window.addEventListener('load', function(e) {
+  var commercBox = document.querySelector('.sidebar-commerc'),
+      commercBoxPosition = commercBox.offsetTop,
+      sideBarBottom = document.querySelector('.sidebar');
 
+  if (window.matchMedia("(min-width: 1091px)").matches) {
+    window.addEventListener('scroll', fixingBlock)
+  } else {
+    removeEventListener('scroll', fixingBlock)
+  }
 
-  var commercBox = document.querySelector('.sidebar-commerc');
+  window.addEventListener('resize', function() {
+    if (window.matchMedia("(min-width: 1091px)").matches) {
+      window.addEventListener('scroll', fixingBlock)
+    } else {
+      removeEventListener('scroll', fixingBlock)
+    }
+  });
 
-// });
-
-window.addEventListener('scroll', function() {
-  commercBox.innerHTML = pageYOffset + 'px';
+  function fixingBlock() {
+    if(pageYOffset >= commercBoxPosition && sideBarBottom.getBoundingClientRect().bottom > commercBox.clientHeight) {
+      commercBox.classList.add('fixed-top');
+    } else {
+      commercBox.classList.remove('fixed-top');
+    }
+  }
 });
+
 
