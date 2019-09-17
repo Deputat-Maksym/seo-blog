275317901367abcc31c36caee06fdd187ea0160b
Максим <deputat_maksym@i.ua>
2019-09-18 00:37:44 +0300

    faq sidebar; some bugs fixed

--- src/js/templates/modal.js
+++ src/js/templates/modal.js
@@ -1,15 +1,24 @@
 var btnModalOpen = document.querySelectorAll('.modal-show'),
+    btnModalClose = document.querySelector('.modal-close'),
     modalWindow = document.querySelector('.modal'),
-    modalInnerLayer = document.querySelector('.modal-inner-layer');
+    modalInnerLayer = document.querySelector('.modal-inner-layer'),
+    htmlBlock = document.querySelector('html');
 
 for(var b = 0; b < btnModalOpen.length; b += 1) {
   btnModalOpen[b].addEventListener('click', function() {
-    modalWindow.classList.add('modal-open')
+    modalWindow.classList.add('modal-open');
+    htmlBlock.style.overflow = 'hidden';
   });
 }
 
 modalInnerLayer.addEventListener('click', function() {
-  modalWindow.classList.remove('modal-open')
+  modalWindow.classList.remove('modal-open');
+  htmlBlock.style.overflow = 'visible';
+});
+
+btnModalClose.addEventListener('click', function() {
+  modalWindow.classList.remove('modal-open');
+  htmlBlock.style.overflow = 'visible';
 });
 
 
